393aaaaa70519a6cf600f387b263c6a6e7da450c
NazarVik <Nazarov_dev@icloud.com>
2022-11-22 17:05:18 +0300

    закончил 4-ю задачу

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,5 +1,15 @@
-﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает 
+// все чётные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write("input N = ");
+int N = int.Parse(Console.ReadLine());
+int count = 2;
+
+while (N <= count) 
+{
+    Console.Write(count + ", ");
+    count += 2;
+}
