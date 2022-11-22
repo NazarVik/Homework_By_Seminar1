4414585ebc2dd1918488d5977673b9b4dbb1b6f8
NazarVik <Nazarov_dev@icloud.com>
2022-11-22 16:47:16 +0300

    закончил вторую задачу

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,4 +3,18 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write("input number1 = ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("input number2 = ");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.Write("input number3 = ");
+int number3 = int.Parse(Console.ReadLine());
+
+int max = number1;
+
+if (number2 > max) max = number2;
+if (number3 > max) Console.WriteLine(number3);
+else Console.WriteLine(max);
